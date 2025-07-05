c6ef5dcfc0826baad33a17551f3253d7b7bbb3d9
Kursat Kilic <kilic_kursat@hotmail.com>
2025-07-05 14:17:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ import re
 # SmolAgent imports
 # from smolagents import tool, ToolCallingAgent, HfApiModel
 # NEW - Correct
-from smolagents import CodeAgent, TransformersModel, DuckDuckGoSearchTool, InferenceClientModel
+from smolagents import CodeAgent, TransformersModel, DuckDuckGoSearchTool, InferenceClientModel, tool
 
 
 # Configure logging
